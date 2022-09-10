2e95471ba2701ecc44ed41f8bd36ea6c29e01287
Guilherme Medeiros <grmfurquim@gmail.com>
2022-09-10 15:51:18 -0300

    Add mensagem de erro caso não carregue os cursos

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
@@ -14,11 +16,20 @@ export class CoursesComponent implements OnInit {
   displayedColumns = ['name', 'category'];
  // coursesService: CoursesService;
 
-  constructor(private coursesService: CoursesService ) {
+  constructor(private coursesService: CoursesService, public dialog: MatDialog ) {
    // this.coursesService = new CoursesService();
-   this.courses$ = this.coursesService.list();
+   this.courses$ = this.coursesService.list()
+   .pipe(
+    catchError(error => {this.onError("Erro ao carregar CoursesComponent."); return of([])})
+   );
 
    }
+
+   onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
 
   ngOnInit(): void {
   }
