dbe3b5e6a89a1befd72b78128281b0eaaa92c390
Guilherme Medeiros <grmfurquim@gmail.com>
2022-09-13 00:44:14 -0300

    Criando controller, testando JPA

--- crud-spring/src/main/java/com/medeiros/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/medeiros/CrudSpringApplication.java
@@ -1,13 +1,29 @@
 package com.medeiros;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.medeiros.model.Course;
+import com.medeiros.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository){
+		return args -> {
+			
+			Course c = new Course();
+			c.setName("Guilherme");
+			c.setCategory("backend");
+			courseRepository.save(c);
+		}; 
 	}
 
 }
---
+++ crud-spring/src/main/java/com/medeiros/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.medeiros.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.medeiros.model.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    
+}
